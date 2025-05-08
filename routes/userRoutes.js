6a3914ea3a454317cf6ba2fc83d689ac4d5f3ddc
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-08 17:39:48 +0530

    created axios 2

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authMiddleware');
-const { 
-    getUserProfile,
-    updateUserProfile,
-    deleteUserProfile
-} = (req, res) => {
-    res.send('User profile');
-};
+const {
+  getUserProfile,
+  updateUserProfile,
+  deleteUserProfile
+} = require('../controllers/userController'); // ✅ FIXED
+
 // User profile routes
 router.get('/profile', auth, getUserProfile);
 router.put('/profile', auth, updateUserProfile);
 router.delete('/profile', auth, deleteUserProfile);
-console.log('userController.getUserProfile:', typeof userController.getUserProfile);
 
 module.exports = router;
