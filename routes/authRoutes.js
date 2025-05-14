4813bee56b2f2054008f4fa98759ed5e4ebc5489
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-14 17:43:23 +0530

    final version 2.5

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { register, login, getProfile } = require('../controllers/authController');
 const { verifyToken } = require('../middleware/authMiddleware');
+const User = require('../models/User');
 
 const router = express.Router();
 
