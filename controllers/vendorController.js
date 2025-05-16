1c274f086ece1d85a195c0071720578b30b34355
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-16 17:05:04 +0530

    version 1

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -4,7 +4,7 @@ const User = require('../models/User');
 // Create a new vendor
 const createVendor = async (req, res) => {
   try {
-    const userId = req.user.id;
+    const userId = req.user._id;
     const user = await User.findById(userId);
 
     if (!user) {
