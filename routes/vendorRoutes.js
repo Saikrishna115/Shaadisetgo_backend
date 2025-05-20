a0ff10f2da93c0f8128dd1ce323c1db06427bbff
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-20 17:42:09 +0530

    added image upload feature V3.3

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verifyToken: protect } = require('../middleware/authMiddleware');
+const { verifyToken: protect, authorize } = require('../middleware/authMiddleware');
 const {
   createVendor,
   getVendors,
